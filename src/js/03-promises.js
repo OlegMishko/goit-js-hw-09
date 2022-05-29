0221c1e11d715c5f2a241eed297b5bcc0c1a6a1e
OlegMishko <oleg.mishko@gmail.com>
2022-05-29 21:56:22 +0300

    js-hw-09v1.0
    Добавлен JS code, задание номер 3. Promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = { formEl: document.querySelector('.form'), };
+refs.formEl.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+  for (let i = 1; i <= amountValue; i += 1) {
+
+    createPromise(i, delayValue).then(({ position, delay }) => Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      .catch(({ position, delay }) => Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
+    delayValue += stepValue;
   }
+}
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
+
 }
